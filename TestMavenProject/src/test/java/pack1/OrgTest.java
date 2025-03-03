700e9395dfea7aa9a712a87bdd10568fe9211d35
suraj <suraj@suraj-pavilion.mshome.net>
2025-03-03 10:35:39 +0530

    1st commit of test maven repo

---
+++ TestMavenProject/src/test/java/pack1/OrgTest.java
@@ -0,0 +1,16 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute createOrgTest");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("execute modifyOrgTest");
+	}
+}
---
+++ TestMavenProject/src/test/java/pack2/ContactTest.java
@@ -0,0 +1,30 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createContactTest() {
+		
+		String Url = System.getProperty("url");
+		//here the user is forgot to provide the data in Cmd line of browser so 
+		//it is taking from the chrome as parameter
+		String Browser = System.getProperty("browser", "chrome");
+		String Username = System.getProperty("username");
+		String Password = System.getProperty("password");
+		
+		
+		System.out.println(Url);
+		System.out.println(Browser);
+		System.out.println(Username);
+		System.out.println(Password);
+		
+		System.out.println("execute createContactTest");
+	}
+	
+	@Test
+	public void modifyContactTest() {
+		System.out.println("execute modifyContactTest");
+	}
+}
